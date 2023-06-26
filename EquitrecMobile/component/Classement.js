57727a00236d0ecb5e6362afeb514fef50db433f
Kenzo Vongkingkeo <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-06-26 14:31:18 +0200

    debut kenzo

---
+++ EquitrecMobile/component/Classement.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Classement() {
+    return (
+        <View style={styles.page}>
+        <Text style={styles.texte}>Classement</Text>
+        </View>
+    );
+
+};
+
+
+const styles = StyleSheet.create({
+
+};

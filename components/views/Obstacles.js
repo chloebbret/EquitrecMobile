dae04607624b8ae8b50735a4ffd3422af74281aa
WizzardHub <contact@wizzardr.dev>
2023-07-04 15:45:22 +0200

    Base app

---
+++ components/views/Obstacles.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+import {StatusBar} from "expo-status-bar";
+
+export default function Obstacles() {
+  return (
+    <View>
+      <Text>Obstacles</Text>
+      <StatusBar />
+    </View>
+  )
+}

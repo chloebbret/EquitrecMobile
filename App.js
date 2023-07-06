61e018ae3f86b79c39f4a9c211f0a74d8d29806d
Kenzo Vongkingkeo <81594327+KurosakiKenzo69@users.noreply.github.com>
2023-07-06 21:37:49 +0200

    push bdd

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet} from 'react-native';
+import {createDrawerNavigator} from "@react-navigation/drawer";
+import {NavigationContainer} from "@react-navigation/native";
+import Accueil from "./components/views/Accueil";
+import Obstacles from "./components/views/Obstacles";
 
-import Accueil from "./component/Accueil.js";
-import Menu from "./component/Menu.js";
+const Drawer = createDrawerNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Accueil></Accueil>
-      <Menu></Menu>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator>
+                <Drawer.Screen name="Accueil" component={Accueil} />
+                <Drawer.Screen name="Obstacles" component={Obstacles} />
+            </Drawer.Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const sqlite3 = require('sqlite3').verbose();
 // Crée une nouvelle instance de la base de données SQLite
 const db = new sqlite3.Database('equitrec.db');
 
-// Exécute les requêtes SQL pour créer les tables et insérer les données
+// Exécute les requêtes SQL pour créer les tables
 db.serialize(() => {
     // Table: competiteur
     db.run(`CREATE TABLE IF NOT EXISTS competiteur (
@@ -17,7 +17,67 @@ db.serialize(() => {
     FOREIGN KEY (niveau) REFERENCES niveau(id_niveau)
   )`);
 
-    // Insertion des données dans la table competiteur
+    // Table: competition
+    db.run(`CREATE TABLE IF NOT EXISTS competition (
+    id_competition INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_competition VARCHAR(50) NOT NULL,
+    debut_competition DATE DEFAULT NULL,
+    fin_competition DATE DEFAULT NULL,
+    nb_epreuves INTEGER NOT NULL,
+    lat_competition FLOAT DEFAULT NULL,
+    lon_competition FLOAT DEFAULT NULL,
+    id_juge INTEGER DEFAULT NULL,
+    FOREIGN KEY (id_juge) REFERENCES juges (id_juge)
+  )`);
+
+    // Table: niveau
+    db.run(`CREATE TABLE IF NOT EXISTS niveau (
+    id_niveau INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle_niveau VARCHAR(50) NOT NULL
+  )`);
+
+    // Table: juge
+    db.run(`CREATE TABLE IF NOT EXISTS juges (
+    id_juge INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_juge VARCHAR(50) NOT NULL,
+    prenom_juge VARCHAR(50) NOT NULL,
+    tel_juge INTEGER NOT NULL,
+    mail_juge VARCHAR(50) NOT NULL,
+    login_juge VARCHAR(50) NOT NULL,
+    pass_juge VARCHAR(50) NOT NULL,
+    id_competition INTEGER NOT NULL,
+    FOREIGN KEY (id_competition) REFERENCES competition(id_competition)
+  )`);
+
+    // Table: epreuve
+    db.run(`CREATE TABLE IF NOT EXISTS epreuve (
+    id_epreuve INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle_epreuve VARCHAR(50) NOT NULL,
+    id_competition INTEGER NOT NULL,
+    FOREIGN KEY (id_competition) REFERENCES competition(id_competition)
+  )`);
+
+    // Table: obstacle
+    db.run(`CREATE TABLE IF NOT EXISTS obstacle (
+    id_obstacle INTEGER PRIMARY KEY AUTOINCREMENT,
+    libelle_obstacle VARCHAR(50) NOT NULL,
+    id_epreuve INTEGER NOT NULL,
+    FOREIGN KEY (id_epreuve) REFERENCES epreuve(id_epreuve)
+  )`);
+
+    // Table: user
+    db.run(`CREATE TABLE IF NOT EXISTS user (
+    id_user INTEGER PRIMARY KEY AUTOINCREMENT,
+    nom_user VARCHAR(50) NOT NULL,
+    prenom_user VARCHAR(50) NOT NULL,
+    tel_user INTEGER NOT NULL,
+    mail_user VARCHAR(50) NOT NULL,
+    login_user VARCHAR(50) NOT NULL,
+    pass_user VARCHAR(50) NOT NULL
+  )`);
+});
+
+db.serialize(() => {
     db.run(`INSERT INTO competiteur (id_competiteur, nom_competiteur, prenom_competiteur, num_licence, id_competition, niveau) VALUES
     (1, 'Jackson', 'Andréa', 12365478, 2, NULL),
     (2, 'Sheperd', 'Amelia', 7896542, 1, NULL),
@@ -31,17 +91,6 @@ db.serialize(() => {
     (10, 'Petit', 'Sophie', 876543, 3, NULL),
     (11, 'Robert', 'Pierre', 210987, 4, NULL)`);
 
-    // Table: competition
-    db.run(`CREATE TABLE IF NOT EXISTS competition (
-    id_competition INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom_competition VARCHAR(50) NOT NULL,
-    debut_competition DATE DEFAULT NULL,
-    fin_competition DATE DEFAULT NULL,
-    nb_epreuves INTEGER NOT NULL,
-    lat_competition FLOAT DEFAULT NULL,
-    lon_competition FLOAT DEFAULT NULL
-  )`);
-
     // Insertion des données dans la table competition
     db.run(`INSERT INTO competition (id_competition, nom_competition, debut_competition, fin_competition, nb_epreuves, lat_competition, lon_competition) VALUES
     (1, 'Compétition 1', '2023-07-01', '2023-07-05', 10, NULL, NULL),
@@ -52,12 +101,6 @@ db.serialize(() => {
     (6, 'test', '2023-04-20', '2023-04-22', 10, NULL, NULL),
     (7, 'Test 2', '2023-04-20', '2023-04-28', 5, NULL, NULL)`);
 
-    // Table: niveau
-    db.run(`CREATE TABLE IF NOT EXISTS niveau (
-    id_niveau INTEGER PRIMARY KEY AUTOINCREMENT,
-    libelle_niveau VARCHAR(50) NOT NULL
-  )`);
-
     // Insertion des données dans la table niveau
     db.run(`INSERT INTO niveau (id_niveau, libelle_niveau) VALUES
     (1, 'Débutant'),
@@ -66,30 +109,10 @@ db.serialize(() => {
     (4, 'Expert'),
     (5, 'Maître')`);
 
-    // Table: juge
-    db.run(`CREATE TABLE IF NOT EXISTS juge (
-    id_juge INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom_juge VARCHAR(50) NOT NULL,
-    prenom_juge VARCHAR(50) NOT NULL,
-    tel_juge INTEGER NOT NULL,
-    mail_juge VARCHAR(50) NOT NULL,
-    login_juge VARCHAR(50) NOT NULL,
-    pass_juge VARCHAR(50) NOT NULL,
-    id_competition INTEGER NOT NULL,
-    FOREIGN KEY (id_competition) REFERENCES competition(id_competition)
-  )`);
-
     // Insertion des données dans la table juge
-    db.run(`INSERT INTO juge (id_juge, nom_juge, prenom_juge, tel_juge, mail_juge, login_juge, pass_juge, id_competition) VALUES
+    db.run(`INSERT INTO juges (id_juge, nom_juge, prenom_juge, tel_juge, mail_juge, login_juge, pass_juge, id_competition) VALUES
     (1, 'Doe', 'John', 1234567890, 'john.doe@example.com', 'johndoe', 'password', 1)`);
 
-    // Table: epreuve
-    db.run(`CREATE TABLE IF NOT EXISTS epreuve (
-    id_epreuve INTEGER PRIMARY KEY AUTOINCREMENT,
-    libelle_epreuve VARCHAR(50) NOT NULL,
-    id_competition INTEGER NOT NULL,
-    FOREIGN KEY (id_competition) REFERENCES competition(id_competition)
-  )`);
 
     // Insertion des données dans la table epreuve
     db.run(`INSERT INTO epreuve (id_epreuve, libelle_epreuve, id_competition) VALUES
@@ -99,14 +122,6 @@ db.serialize(() => {
     (4, 'Épreuve 4', 2),
     (5, 'Épreuve 5', 3)`);
 
-    // Table: obstacle
-    db.run(`CREATE TABLE IF NOT EXISTS obstacle (
-    id_obstacle INTEGER PRIMARY KEY AUTOINCREMENT,
-    libelle_obstacle VARCHAR(50) NOT NULL,
-    id_epreuve INTEGER NOT NULL,
-    FOREIGN KEY (id_epreuve) REFERENCES epreuve(id_epreuve)
-  )`);
-
     // Insertion des données dans la table obstacle
     db.run(`INSERT INTO obstacle (id_obstacle, libelle_obstacle, id_epreuve) VALUES
     (1, 'Obstacle 1', 1),
@@ -115,86 +130,74 @@ db.serialize(() => {
     (4, 'Obstacle 4', 2),
     (5, 'Obstacle 5', 3)`);
 
-    // Table: user
-    db.run(`CREATE TABLE IF NOT EXISTS user (
-    id_user INTEGER PRIMARY KEY AUTOINCREMENT,
-    nom_user VARCHAR(50) NOT NULL,
-    prenom_user VARCHAR(50) NOT NULL,
-    tel_user INTEGER NOT NULL,
-    mail_user VARCHAR(50) NOT NULL,
-    login_user VARCHAR(50) NOT NULL,
-    pass_user VARCHAR(50) NOT NULL
-  )`);
-
     // Insertion des données dans la table user
     db.run(`INSERT INTO user (id_user, nom_user, prenom_user, tel_user, mail_user, login_user, pass_user) VALUES
     (1, 'Smith', 'Jane', 9876543210, 'jane.smith@example.com', 'janesmith', 'password')`);
+});
 
-    // Récupère les données de la table competiteur
-    db.all(`SELECT * FROM competiteur`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_competiteur, row.nom_competiteur, row.prenom_competiteur, row.num_licence, row.id_competition, row.niveau);
-            });
-        }
-    });
+db.all(`SELECT * FROM competiteur`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_competiteur, row.nom_competiteur, row.prenom_competiteur, row.num_licence, row.id_competition, row.niveau);
+        });
+    }
+});
 
-    // Récupère les données de la table competition
-    db.all(`SELECT * FROM competition`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_competition, row.nom_competition, row.debut_competition, row.fin_competition, row.nb_epreuves, row.lat_competition, row.lon_competition);
-            });
-        }
-    });
+// Récupère les données de la table competition
+db.all(`SELECT * FROM competition`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_competition, row.nom_competition, row.debut_competition, row.fin_competition, row.nb_epreuves, row.lat_competition, row.lon_competition);
+        });
+    }
+});
 
-    // Récupère les données de la table juge
-    db.all(`SELECT * FROM juge`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_juge, row.nom_juge, row.prenom_juge, row.tel_juge, row.mail_juge, row.login_juge, row.pass_juge, row.id_competition);
-            });
-        }
-    });
+// Récupère les données de la table juge
+db.all(`SELECT * FROM juges`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_juge, row.nom_juge, row.prenom_juge, row.tel_juge, row.mail_juge, row.login_juge, row.pass_juge, row.id_competition);
+        });
+    }
+});
 
-    // Récupère les données de la table epreuve
-    db.all(`SELECT * FROM epreuve`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_epreuve, row.libelle_epreuve, row.id_competition);
-            });
-        }
-    });
+// Récupère les données de la table epreuve
+db.all(`SELECT * FROM epreuve`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_epreuve, row.libelle_epreuve, row.id_competition);
+        });
+    }
+});
 
-    // Récupère les données de la table obstacle
-    db.all(`SELECT * FROM obstacle`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_obstacle, row.libelle_obstacle, row.id_epreuve);
-            });
-        }
-    });
+// Récupère les données de la table obstacle
+db.all(`SELECT * FROM obstacle`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_obstacle, row.libelle_obstacle, row.id_epreuve);
+        });
+    }
+});
 
-    // Récupère les données de la table user
-    db.all(`SELECT * FROM user`, (err, rows) => {
-        if (err) {
-            console.error(err);
-        } else {
-            rows.forEach((row) => {
-                console.log(row.id_user, row.nom_user, row.prenom_user, row.tel_user, row.mail_user, row.login_user, row.pass_user);
-            });
-        }
-    });
+// Récupère les données de la table user
+db.all(`SELECT * FROM user`, (err, rows) => {
+    if (err) {
+        console.error(err);
+    } else {
+        rows.forEach((row) => {
+            console.log(row.id_user, row.nom_user, row.prenom_user, row.tel_user, row.mail_user, row.login_user, row.pass_user);
+        });
+    }
 });
 
 // Ferme la connexion à la base de données
